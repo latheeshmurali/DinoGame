c27e577c22ca4e525529bec81b589df46e8e8368
latheesh <latheeshmurali07@gmail.com>
2023-11-26 16:05:01 +0530

    trail commit

--- lib/main.dart
+++ lib/main.dart
@@ -38,10 +38,10 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
 
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ), 
+      // appBar: AppBar(
+      //   backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      //   title: Text(widget.title),
+      // ), 
     );
   }
 }
